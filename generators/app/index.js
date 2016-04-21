8b4fd13f783171c136463234d3a91bdb57e2c80d
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2016-04-21 01:22:24 +0100

    start testing things

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,39 +3,56 @@ var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
 
-module.exports = yeoman.Base.extend({
+class Generator extends yeoman.Base {
 
-  prompting: function () {
-    var done = this.async();
+  get initializing() {
+    return {
+      checkEnvironment() {
+        if (!this._checkIfRNIsInstalled()) {
+          this.env.error("something bad is happened");
+        }
+      }
+    };
+  }
+
+  prompting() {
+    // var done = this.async();
 
     // Have Yeoman greet the user.
     this.log(yosay(
       'Welcome to the sublime ' + chalk.red('generator-rn') + ' generator!'
     ));
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+    // var prompts = [{
+    //   type: 'confirm',
+    //   name: 'someAnswer',
+    //   message: 'Would you like to enable this option?',
+    //   default: true
+    // }];
 
-    this.prompt(prompts, function (props) {
-      this.props = props;
-      // To access props later use this.props.someAnswer;
+    // this.prompt(prompts, function (props) {
+    //   this.props = props;
+    //   // To access props later use this.props.someAnswer;
 
-      done();
-    }.bind(this));
-  },
+    //   done();
+    // }.bind(this));
+  }
 
-  writing: function () {
+  writing() {
     this.fs.copy(
       this.templatePath('dummyfile.txt'),
       this.destinationPath('dummyfile.txt')
     );
   }
 
+  _checkIfRNIsInstalled() {
+    console.error('checking if RN is installed');
+    return false;
+  }
+
   // install: function () {
   //   this.installDependencies();
   // }
-});
+}
+
+module.exports = Generator;
--- test/app.js
+++ test/app.js
@@ -1,17 +1,42 @@
+/* eslint no-unused-vars: 0, no-unused-expressions:0 */
+
 import path from 'path';
 import assert from 'yeoman-assert';
 import helpers from 'yeoman-test';
+import chai from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+
+const expect = chai.expect;
 
 describe('generator-rn:app', () => {
+  let _checkIfRNIsInstalledStub = null;
+
   before(done => {
     helpers.run(path.join(__dirname, '../generators/app'))
       .withPrompts({someAnswer: true})
+      .on('ready', function (generator) {
+        _checkIfRNIsInstalledStub =
+          sinon.stub(generator, '_checkIfRNIsInstalled').returns(true);
+      })
       .on('end', done);
+  });
+
+  after(() => {
+    _checkIfRNIsInstalledStub && _checkIfRNIsInstalledStub.restore();
   });
 
   it('creates files', () => {
     assert.file([
       'dummyfile.txt'
     ]);
+  });
+
+  it('checks if react-native is installed', () => {
+    expect(_checkIfRNIsInstalledStub.calledOnce).to.be.ok;
+  });
+
+  it('aborts setup if RN is not installed', () => {
+
   });
 });
