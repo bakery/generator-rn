70b6941bbafc5cb1d2d4505efb79b27d6073dfb2
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2016-05-24 19:47:23 +0100

    upgrade to RN 0.26.1

--- generators/app/index.js
+++ generators/app/index.js
@@ -70,10 +70,12 @@ module.exports = BaseGenerator.extend({
           },
           scripts: {
             'build-ios': 'node node_modules/react-native/local-cli/cli.js bundle --entry-file index.ios.js --bundle-output iOS/main.jsbundle --platform "ios" --assets-dest ./  --dev false --reset-cache',
-            'build-android': 'node node_modules/react-native/local-cli/cli.js bundle --entry-file index.android.js --bundle-output iOS/main.jsbundle --platform "android" --assets-dest ./  --dev false --reset-cache'
+            'build-android': 'node node_modules/react-native/local-cli/cli.js bundle --entry-file index.android.js --bundle-output iOS/main.jsbundle --platform "android" --assets-dest ./  --dev false --reset-cache',
+            'ios': 'node node_modules/react-native/local-cli/cli.js run-ios'
           },
           dependencies: {
-            'react-native': '^0.24.1',
+            'react': '15.0.2',
+            'react-native': '^0.26.1',
             'react-redux': '^4.4.5',
             'redux': '^3.5.2',
             'redux-immutable': '^3.0.6',
--- generators/app/templates/app/setup.js
+++ generators/app/templates/app/setup.js
@@ -1,5 +1,5 @@
 import App from './components/App';
-import React from 'React';
+import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import configureStore from './store';
 
