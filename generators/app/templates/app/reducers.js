b098274bf5c6fdc909e32941d03dbffa98e4bcff
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2016-05-24 19:47:39 +0100

    add a bogus reducer so the app runs right away

--- generators/app/templates/app/reducers.js
+++ generators/app/templates/app/reducers.js
@@ -3,6 +3,7 @@ import { combineReducers } from 'redux-immutable';
 // XX: Do not rename this variable if you want reducer generator
 // to keep working properly (and you do want that, right?)
 const applicationReducers = {
+  removeThisReducerOnceYouAddALegitOne: () => ({}),
 };
 
 export default function createReducer() {
